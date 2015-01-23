1ccdea1adaf15e9746d0c4183effb2982dcfb997
Kirk Backus <kirkbackus@gmail.com>
2015-01-23 10:34:33 -0600

    Used the ip instead of another domain.

--- rabbitmq_cluster/recipes/default.rb
+++ rabbitmq_cluster/recipes/default.rb
@@ -2,7 +2,7 @@
 instances = node[:opsworks][:layers][:rabbitmq][:instances]
 
 instances.each do |name, attrs|
-  hostsfile_entry attrs['public_dns_name'] do
+  hostsfile_entry attrs['ip'] do
     hostname  name
     unique    true
   end
