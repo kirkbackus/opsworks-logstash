c5d7661a497d15e23bda4c17a01d59f17bf73731
Kirk Backus <kirkbackus@gmail.com>
2015-01-23 10:53:39 -0600

    Clustering, revert private_ip and use -ext in name

--- rabbitmq_cluster/recipes/default.rb
+++ rabbitmq_cluster/recipes/default.rb
@@ -2,13 +2,13 @@
 instances = node[:opsworks][:layers][:rabbitmq][:instances]
 
 instances.each do |name, attrs|
-  hostsfile_entry attrs['ip'] do
+  hostsfile_entry attrs['private_ip'] do
     hostname  name
     unique    true
   end
 end
 
-rabbit_nodes = instances.map{ |name, attrs| "rabbit@#{name}" }
+rabbit_nodes = instances.map{ |name, attrs| "rabbit@#{name}-ext" }
 node.set['rabbitmq']['cluster_disk_nodes'] = rabbit_nodes
 
 include_recipe 'rabbitmq'
